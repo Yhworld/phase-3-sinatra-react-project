0490d69cb9f34de860ae669e1a6eb90cb2d749fc
Yahya <yahyahussein670@gmail.com>
2022-09-06 12:29:26 +0300

    Update the user.rb file by adding a relationship between the user and post

--- db/migrate/20220906090852_create_users.rb
+++ db/migrate/20220906090852_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :profile_image
+      t.datetime :created_at
+      t.datetime :updated_at
   end
 end
