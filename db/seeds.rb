ae54e16fa2672afaa1df6bfc35975f957a6e0975
Yahya <yahyahussein670@gmail.com>
2022-09-08 08:58:09 +0300

    Add a dummy content in the seed file to populate the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,20 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+user = User.create(
+      username: "Liza",
+      email: "liza@example.com",
+      password: "1234567890",
+      profile_image: "http://example.com/profile_image",
+    )
+
+  post = Post.create(
+          location: "http://example.com/profile_image",
+          image: "http://example.com/profile_image",
+          caption: "this is a caption",
+          likes: 20,
+          comments: "soooo cool",
+          user_id: user.id,
+        )
+
 puts "✅ Done seeding!"
