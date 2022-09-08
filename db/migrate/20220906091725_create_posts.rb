38c77a5b59cd09b660fe171d4fe6def07023b110
Yahya <yahyahussein670@gmail.com>
2022-09-08 09:02:28 +0300

    update the post table by adding an image, userid and a caption column

--- db/migrate/20220906091725_create_posts.rb
+++ db/migrate/20220906091725_create_posts.rb
@@ -1,10 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :location
+      t.string :image
       t.string :caption
       t.integer :likes
+      t.integer :comments
+      t.integer :user_id
       t.datetime :created_at
       t.datetime :updated_at
+    end
   end
 end
