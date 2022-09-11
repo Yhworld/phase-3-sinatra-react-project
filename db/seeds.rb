b998e21af4aa19024eab06c37bfe22f7421c3346
Yahya <yahyahussein670@gmail.com>
2022-09-11 12:54:07 +0300

    updated the image link in post

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ user = User.create(
 
   post = Post.create(
           location: "http://example.com/profile_image",
-          image: "http://example.com/profile_image",
+          image: "https://images.unsplash.com/photo-1523510468197-455cc987be86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
           caption: "this is a caption",
           likes: 20,
           comments: "soooo cool",
