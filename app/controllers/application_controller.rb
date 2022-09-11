d0ad9f43feb3b39fea736e4a7fc593a8aa41261b
Yahya <yahyahussein670@gmail.com>
2022-09-11 12:50:02 +0300

    Add a login path which grabs a users email and password from the users table and compares whether they match

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -77,6 +77,19 @@ class ApplicationController < Sinatra::Base
       end
     end
 
+    post "/login" do
+      user = User.find_by_email(params[:email])
+      if user && user.authenticate(params[:password])
+        
+          session[:user_id] = user.id
+          
+    
+          halt 200, {user: user, message:"User successfully logged in"}.to_json
+      else
+          halt 404, {error: "Invalid information"}.to_json
+      end
+    end
+
 
     
 
